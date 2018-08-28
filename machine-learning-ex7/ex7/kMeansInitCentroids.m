b06b755755b2d600179bef0d1fb19d4c738e9105
WK <710290912@qq.com>
2018-08-28 12:31:03 +0800

    finish week7

---
+++ machine-learning-ex7/ex7/kMeansInitCentroids.m
@@ -0,0 +1,25 @@
+function centroids = kMeansInitCentroids(X, K)
+%KMEANSINITCENTROIDS This function initializes K centroids that are to be 
+%used in K-Means on the dataset X
+%   centroids = KMEANSINITCENTROIDS(X, K) returns K initial centroids to be
+%   used with the K-Means on the dataset X
+%
+
+% You should return this values correctly
+centroids = zeros(K, size(X, 2));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: You should set centroids to randomly chosen examples from
+%               the dataset X
+%
+
+%randperm（N,M） 返回M个互不重复的从1：N的随机整数
+randidx = randperm(size(X,1), K);
+centroids = X(randidx,:);
+
+
+
+% =============================================================
+
+end
+
